ea0410ddd12bd8671c358e951d2179b3f9d551c8
fabibot <fabulousfab.bellon@gmail.com>
2024-03-20 11:47:45 -0500

    excercice terminée

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 let caseClicked = "";
 let playerCaseList = [];
 let computerCaseList = [];
+let arrayPlayerShips = [];
+
 
 const playButton = document.querySelector(".divButton>button");
 playButton.addEventListener("click", () => {
     resetGame();
 });
 
-let playerShipsObject = [{coordonée : 'I9', orientation: "v"}, {coordonée : "D8", orientation: 'h'},
-{coordonée : "G2", orientation: "h"}, {coordonée : "G4", orientation : "v"},
-{coordonée :"B3", orientation : "v"}];
-let computerShipsObject = [{coordonée : 'D4', orientation: "v"}, {coordonée : "F2", orientation: 'h'},
-{coordonée : "B8", orientation: "v"}, {coordonée : "J2", orientation : "v"},
-{coordonée :"D7", orientation : "h"}];
+
+let computerShipsObject = [{coordonée : 'D4', orientation: "v", lenght : 1}, {coordonée : "F2", orientation: 'h', lenght : 2},
+{coordonée : "B8", orientation: "v", lenght : 3}, {coordonée : "J2", orientation : "v", lenght : 4},
+{coordonée :"D7", orientation : "h", lenght : 5}];
 
 class Ship {
     constructor(lenght, position){
@@ -37,15 +37,16 @@ class Ship {
 
 class Gameboard {
     constructor(playerArray, computerArray) {
-        this.playerShips = this.placeTheShips(playerArray);
-        this.computerShips = this.placeTheShips(computerArray);
+        this.playerShips = this.setTheShips(playerArray);
+        this.computerShips = this.setTheShips(computerArray);
         this.attackList = [];
     }
-    placeTheShips(array) {
+
+    setTheShips(array) {
         let positionShips  = [];
         for(let position of array){
             let index = array.indexOf(position);
-            let newShip = new Ship(index + 1, position.coordonée);
+            let newShip = new Ship(position.lenght, position.coordonée);
             if(position.orientation == "v") {
                 newShip.isVertical = true;
             }
@@ -119,7 +120,7 @@ class Gameboard {
 }
 
 class ManageDOM {
-    displayGrid(){
+    displayGrid(comp){
         let node = document.createElement("div");
         node.classList.add("board");
         let divLetter = document.createElement("div");
@@ -128,6 +129,9 @@ class ManageDOM {
         divLetter.classList.add("letters");
         divNumber.classList.add("numbers");
         divgrid.classList.add("grid");
+        if(comp){
+            divgrid.classList.add("comp");
+        }
         node.appendChild(divLetter);
         node.appendChild(divNumber);
         node.appendChild(divgrid);
@@ -166,21 +170,9 @@ class ManageDOM {
         const divButton = document.querySelector(".divButton");
         let h2 = document.createElement("h2");
         h2.textContent = "Place your ships (press R to reverse ship)";
-        let readyButton = document.createElement("button");
-        readyButton.textContent = "ready to fight !";
-        readyButton.addEventListener("click", () => {
-            let buttonPlay = document.createElement("button");
-            buttonPlay.textContent = "replay";
-            buttonPlay.addEventListener("click", () => {
-                resetGame();
-            });
-            divButton.replaceChild(buttonPlay, readyButton);
-            divButton.removeChild(h2);
-            startGame();
-        })
+       //////////*******************************************************///////// */
         let oldDiv = divButton.children;
         divButton.replaceChild(h2, oldDiv[0]);
-        divButton.appendChild(readyButton);
         const mainDiv = document.querySelector(".mainDiv");
         const gameBoard = document.createElement("div");
         gameBoard.classList.add("gameboard");
@@ -204,14 +196,14 @@ class ManageDOM {
         compDiv.classList.add("side", "right"); 
         let h3comp = document.createElement("h3");
         h3comp.textContent = "computer Grid";
-        let node2 = this.displayGrid();
+        let node2 = this.displayGrid("comp");
         compDiv.appendChild(h3comp);
         compDiv.appendChild(node2);
         gameBoard.appendChild(compDiv);
     
         const allGrids = document.querySelectorAll(".grid");
         this.setGrid(allGrids[0]);
-        this.setGrid(allGrids[1]);
+        this.setGrid(allGrids[1], "comp");
         this.displaySides();
         this.placePlayerShips();
 
@@ -219,18 +211,17 @@ class ManageDOM {
 
     placePlayerShips(){
         let caseGridList = document.querySelectorAll(".grid > div");
-        let positionList = [];
         let isShipVertical = false;
-        let squareSizes = ['60px', '90px', '120px', '150px', '180px']
+        let squareSizes = ['70px', '105px', '140px', '175px'];
         let index = 0;
         const square = document.createElement('div');
         square.classList.add('square');
         document.body.appendChild(square);
         document.addEventListener('mousemove', (event) => {
-        const mouseX = event.clientX;
-        const mouseY = event.clientY;
-        square.style.left = mouseX - 15 + 'px';
-        square.style.top = mouseY - 15 +'px';
+            const mouseX = event.clientX;
+            const mouseY = event.clientY;
+            square.style.left = mouseX - 15 + 'px';
+            square.style.top = mouseY - 15 +'px';
         });
         window.addEventListener("keydown", (e) =>{
             let squareWidth = getComputedStyle(square).width;
@@ -250,23 +241,48 @@ class ManageDOM {
         });
         for(let i = 0; i < 100; i++){
             caseGridList[i].addEventListener('click', () =>{
-                caseGridList[i].classList.add("shipAlive");
                 let id = caseGridList[i].getAttribute("id");
+                let orientation;
                 if(isShipVertical){
-                    square.style.height = squareSizes[index];  //todo : : arranger l'aggrandissement PROGRESSIF de ca
+                    square.style.height = squareSizes[index];
+                    orientation = "v";
+                    //todo : : arranger l'aggrandissement PROGRESSIF de ca
                 } else {
                     square.style.width = squareSizes[index];
+                    orientation = "h";
                 }
-                index++
-                console.log(square.style.height);
-                positionList.push(id);
-                console.log(positionList);
+                index++;
+                let objectShip = {coordonée : id, orientation : orientation, lenght : index}; 
+                arrayPlayerShips.push(objectShip);
+                this.displayShips(objectShip);
+                if(index == 5){
+                    for(let caseGrid of caseGridList){
+                        caseGrid.style.pointerEvents = "none";
+                    }
+                    document.body.removeChild(square);
+                    const divButton = document.querySelector(".divButton");
+                    let readyButton = document.createElement("button");
+                    readyButton.textContent = "ready to fight !";
+                    readyButton.addEventListener("click", () => {
+                        let buttonPlay = document.createElement("button");
+                        buttonPlay.textContent = "replay";
+                        buttonPlay.addEventListener("click", () => {
+                            resetGame();
+                        });
+                        divButton.replaceChild(buttonPlay, readyButton);
+                        const h2 = document.querySelector("h2");
+                        divButton.removeChild(h2);
+                        startGame();
+                    });
+                    divButton.appendChild(readyButton);
+
+                } 
+                  
             });
         }
-
     }
 
-    setGrid(grid){
+    setGrid(grid, isComputerGrid){
         let letter = "A";
         let number = 1;
         let position = "";
@@ -280,39 +296,40 @@ class ManageDOM {
                 number++;
             }
             gridCase.setAttribute("id", position);
-            gridCase.addEventListener("click", () => {
-                caseClicked = gridCase.getAttribute('id');
-            });
+            if(isComputerGrid){
+                gridCase.addEventListener("click", () => {
+                    caseClicked = gridCase.getAttribute('id');
+                });
+            }
             grid.appendChild(gridCase);
         }
     }
-    displayShips(ships){
-        let currentCase = "";
-        for(let ship of ships) {
-            if(ship.isVertical) {
-                let letter = ship.position[0];
-                    let number = ship.position[1];
-                    let lenght = ship.lenght;
-                    for(let i = 0; i < lenght; i++){
-                        currentCase = letter + number;
-                        number++;
-                        let caseShip = document.querySelector(`#${currentCase}`);
-                        caseShip.classList.add("shipAlive")
-                    }
-            } else {
-                let letter = ship.position[0];
-                let number = ship.position[1];
+    displayShips(ship){
+        let currentCase = '';
+        if(ship.orientation == "v") {
+            let letter = ship.coordonée[0];
+                let number = ship.coordonée[1];
                 let lenght = ship.lenght;
-                for(let i = 0; i < lenght; i++) {
-                    let newLetter = String.fromCharCode(letter.charCodeAt(0) + 1);
+                for(let i = 0; i < lenght; i++){
                     currentCase = letter + number;
-                    letter = newLetter;
+                    number++;
                     let caseShip = document.querySelector(`#${currentCase}`);
                     caseShip.classList.add("shipAlive")
                 }
+        } else {
+            let letter = ship.coordonée[0];
+            let number = ship.coordonée[1];
+            let lenght = ship.lenght;
+            for(let i = 0; i < lenght; i++) {
+                let newLetter = String.fromCharCode(letter.charCodeAt(0) + 1);
+                currentCase = letter + number;
+                letter = newLetter;
+                let caseShip = document.querySelector(`#${currentCase}`);
+                caseShip.classList.add("shipAlive")
             }
         }
-    } 
+            
+        } 
 }
 
 let dom = new ManageDOM();
@@ -321,40 +338,45 @@ function resetGame(){ // appelé si play est clické!
     console.log("start game!");
     playerCaseList = [];
     computerCaseList = [];
+    arrayPlayerShips = [];
     dom.displayGameBoard();
 }
 
 function startGame(){ //appelé si ready to fight est clické
-    let game = new Gameboard(playerShipsObject, computerShipsObject);
-    dom.displayShips(game.playerShips);
-    const rightDiv = document.querySelector(".right");
+    let game = new Gameboard(arrayPlayerShips, computerShipsObject);
+    const rightGridDiv = document.querySelectorAll(".comp>div");
+    rightGridDiv.forEach((element) => element.style.pointerEvents = "");
+    rightGridDiv.forEach((element) => element.classList.add("hover-effect"));
+    const rightDiv = document.querySelector(".comp");
     rightDiv.addEventListener("click", () => {
-        if(!playerCaseList.includes(caseClicked)){
-            playerCaseList.push(caseClicked);
-            playerCaseList.push(caseClicked);
-            let isHit = game.receiveAttack(caseClicked, game.computerShips);
-            const caseClickedDiv = document.querySelectorAll((`#${caseClicked}`));
-            if(isHit){
-                caseClickedDiv[1].classList.add("shipSunk");
-            } else {
-                caseClickedDiv[1].classList.add("clicked");
-            }
-            if(game.checkIfWinner(game.computerShips)){
-                dom.printWinner("Player");
-            }
-            setTimeout(() => {
-                let computerAttack = game.getComputerAttack();
-                let isHit = game.receiveAttack(computerAttack, game.playerShips);
-                const caseClickedDiv = document.querySelectorAll((`#${computerAttack}`));
+        if(caseClicked){
+            if(!playerCaseList.includes(caseClicked)){
+                playerCaseList.push(caseClicked);
+                playerCaseList.push(caseClicked);
+                let isHit = game.receiveAttack(caseClicked, game.computerShips);
+                const caseClickedDiv = document.querySelectorAll((`#${caseClicked}`));
                 if(isHit){
-                    caseClickedDiv[0].classList.add("shipSunk");
+                    caseClickedDiv[1].classList.add("shipSunk");
                 } else {
-                    caseClickedDiv[0].classList.add("clicked");
+                    caseClickedDiv[1].classList.add("clicked");
                 }
-                if(game.checkIfWinner(game.playerShips)){
-                    printWinner("Cumputer");
+                if(game.checkIfWinner(game.computerShips)){
+                    dom.printWinner("Player");
                 }
-            }, 700);
+                setTimeout(() => {
+                    let computerAttack = game.getComputerAttack();
+                    let isHit = game.receiveAttack(computerAttack, game.playerShips);
+                    const caseClickedDiv = document.querySelectorAll((`#${computerAttack}`));
+                    if(isHit){
+                        caseClickedDiv[0].classList.add("shipSunk");
+                    } else {
+                        caseClickedDiv[0].classList.add("clicked");
+                    }
+                    if(game.checkIfWinner(game.playerShips)){
+                        printWinner("Cumputer");
+                    }
+                }, 700);
+            }
         }
     });
 }
